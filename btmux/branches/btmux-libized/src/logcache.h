f5cb43098c9651288223c52fc2a3c806ab86b9fa
cu5 <cu5@e60b412a-5d77-420b-80c2-6cef64185626>
2007-04-21 22:21:19 +0000

    Removed obsolete btmux-libized branch.

--- btmux/branches/btmux-libized/src/logcache.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LOGCACHE_H__
-#define __LOGCACHE_H__
-
-void logcache_list(dbref player);
-void logcache_init();
-void logcache_destruct();
-int logcache_writelog(char *fname, char *fdata);
-
-#endif
